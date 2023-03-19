d630bea24ed254f92699e85db58bcc8decdb9dd9
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-03-19 13:07:32 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,14 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+const changeColor = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+const body = document.body;
+
+changeColor.addEventListener("click", () => {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+});
